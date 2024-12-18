142342ac4ce0a491756bc0fefcb807897c7f4bc2
Anderson Rodriguez <andersoncamilo.rodriguez.s@gmail.com>
2024-12-18 13:58:05 -0500

    Implementacion de metodos mostrarMenu, validarentrada de Menu junto con implementacion en la clase Main

--- src/com/aluracursos/peliculasswapi/modelos/Menu.java
+++ src/com/aluracursos/peliculasswapi/modelos/Menu.java
@@ -1,4 +1,38 @@
 package com.aluracursos.peliculasswapi.modelos;
 
+import java.util.Scanner;
+
 public class Menu {
+    Scanner scanner = new Scanner(System.in);
+
+    public void bienvenidaMenu(){
+        System.out.println("""
+                
+                ******************** Bienvenido a Peliculas SWAPI ********************
+                **********************************************************************
+                Aqui podras encontrar detalles acerca de las peliculas de Star Wars.
+                
+                """);
+    }
+
+    public String mostrarMenu(){
+        do{
+            System.out.println("Ingresa el Numero de Episodio que deseas consultar. (Para Salir Escribe \"salir\").");
+            String EleccionUsuario = scanner.nextLine().trim().toLowerCase();
+            if(validarEntradaUsuario(EleccionUsuario)){
+                System.out.println("Ha ingresado una opcion invalida, intentelo de nuevo por favor");
+                continue;
+            }
+            return EleccionUsuario;
+        } while(true);
+    }
+
+    public boolean validarEntradaUsuario(String EleccionUsuario){
+
+        if(EleccionUsuario.matches("\\d+") || EleccionUsuario.contains("salir")){
+            return false;
+        }
+        return true;
+
+    }
 }
--- src/com/aluracursos/peliculasswapi/principal/Main.java
+++ src/com/aluracursos/peliculasswapi/principal/Main.java
@@ -1,7 +1,21 @@
 package com.aluracursos.peliculasswapi.principal;
 
+import com.aluracursos.peliculasswapi.modelos.Menu;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Menu menu = new Menu();
+
+        menu.bienvenidaMenu();
+
+        while(true){
+            String eleccionUsuario = menu.mostrarMenu();
+
+            if(eleccionUsuario.contains("salir")){
+                break;
+            }
+        }
+        System.out.println("Gracias por utilizar nuestros servicios");
+
     }
 }
